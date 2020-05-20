02ef314ad125f8ee2005fb69f908b69dc5b9a7c8
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-05-20 19:21:46 -0400

    fixed errors

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,22 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract Token{
-    string public name = "Dapp Token";
+    string public name = "Elisha Token";
     string public symbol = "DAPP";
     uint256 public totalSupply = 1000000000000000000;
     uint8 public decimals = 10;
     
-    event Transfer{
+    event Transfer(
         address indexed _from,
         address indexed _to,
-        uint256 _value
-    };
+        uint256 _valueq
+    );
 
-    event Approval{
+    event Approval(
         address indexed _owner,
         address indexed _spender,
         uint256 _value
-    };
+    );
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
