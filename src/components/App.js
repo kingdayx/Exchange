454747f3c4be299aff1626b5ca098003ca8b65fa
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-06-13 01:26:18 -0400

    created ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,91 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import EthSwap from '../abis/EthSwap.json';
+import Token from '../abis/Token.json';
+import NavBar from './NavBar';
+import Main from './Main';
 import './App.css';
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ethBalance})
+    // load token
+    const networkId = await web3.eth.net.getId()
+    const tokenData = Token.networks[networkId]
+    if(tokenData){
+    const token = new web3.eth.Contract(Token.abi, tokenData.address)
+    this.setState({token})
+    let tokenBalance =await token.methods.balanceOf(this.state.account).call()
+    this.setState({tokenBalance})
+    }else{
+      window.alert('Token Contract not deployed to connected network')
+    }
+
+    // load ethswap
+    const ethSwapData = EthSwap.networks[networkId]
+    if(ethSwapData){
+    const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+    this.setState({ethSwap})
+    this.setState({ loading:false })
+    }
+  }
+
+  async loadWeb3()  {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      account:'',
+      ethBalance: 0,
+      token:{},
+      tokenBalance:0,
+      ethSwap: {},
+      loading: true
+    }
+
+  }
+
   render() {
+    let content
+    if(this.state.loading){
+      content = <p id="loader" className="text-center" >Loading...</p>
+    }else{
+      content = <Main/>
+    }
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+       <NavBar account= {this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto">
               <div className="content mr-auto ml-auto">
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+               {content}
+               
               </div>
             </main>
           </div>
